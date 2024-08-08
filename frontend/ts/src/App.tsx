a6fd35cd5dfddc14d15cf7e0b0f3e6a039aa953d
bipul077 <bipulratna07@gmail.com>
2024-08-08 21:21:18 +0545

    css complete

--- frontend/ts/src/App.tsx
+++ frontend/ts/src/App.tsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { dummyData } from './data/todos'
+import TodoItem from './components/TodoItem'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const setTodoCompleted = (id:number,completed:boolean)=>{
+    alert(`${id} has been ${completed?"completed":"not completed"}`)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className='py-10 h-screen space-y-5'>
+      <h1 className="font-bold text-3xl text-center">Your Todos</h1>
+      <div className='max-w-lg mx-auto bg-slate-100 rounded-md p-5'>
+        <div className='space-y-2'>
+          {dummyData.map((all,index)=>(
+            <TodoItem todo={all} key={index} onCompletedChange={setTodoCompleted}/>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 
---
+++ frontend/ts/src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData:Todo[] = [
+    {
+        id: 1,
+        title: "Virat Kohli",
+        completed: false
+    },
+    {
+        id: 2,
+        title: "Rohit Sharma",
+        completed: false
+    },
+    {
+        id: 3,
+        title: "Kl Rahul",
+        completed: true
+    }
+]
